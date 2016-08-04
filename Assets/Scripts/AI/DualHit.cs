5a356aa36d91c64f6eac66552e02c895d759381e
unknown <nwuser@GM2150018.da.iwasaki.ac.jp>
2016-08-04 15:21:53 +0900

    複数回ヒットのスクリプト

---
+++ Assets/Scripts/AI/DualHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hit : MonoBehaviour {
+	int cnt;
+	// Use this for initialization
+	void Start () {
+		cnt = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision other){
+		cnt++;
+		if (cnt == 2) {
+			Destroy (this.gameObject);
+			cnt = 0;
+		}
+	}
+}
