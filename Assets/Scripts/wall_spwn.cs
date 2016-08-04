dc1b8859a976ffa6433b98e884dce7393978d0ae
IK <xkira42x@gmail.com>
2016-08-04 16:39:55 +0900

    壁をスクリプトで生成するよう更新

---
+++ Assets/Scripts/wall_spwn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wall_spwn : MonoBehaviour {
+	public GameObject wall;
+	double wall_spc=0.5;
+	float x;
+	float y;
+	// Use this for initialization
+	void Start () {
+		for(x=-3f;x<=3f;x+=(float)wall_spc){
+			for(y=-4.75f;y<=4.75f;y+=(float)wall_spc){
+				if((x==-3f)||(x==3f)||(y==-4.75f)||(y==4.75f)){
+					transform.position=new Vector3(x,y,1);
+					Instantiate(wall,transform.position,transform.rotation);
+				}
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
