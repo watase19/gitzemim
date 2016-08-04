50357cc92d83b25a539c8b477574cb1c269c4805
unknown <nwuser@GM2150026.da.iwasaki.ac.jp>
2016-08-04 15:19:14 +0900

    移動完了、当たり判定途中

---
+++ Assets/MyScenes/idou.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class idou : MonoBehaviour {
+	public GameObject player;
+	public Vector3 playerp;
+	// Use this for initialization
+	void Start () {
+		playerp = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+				playerp.x+=0.1f;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+				playerp.x-=0.1f;
+		}
+		player.transform.position = playerp;
+	}
+}
---
+++ Assets/MyScenes/tama.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tama : MonoBehaviour {
+	public GameObject ball;
+	public Vector3 ballp;
+	int ballflgx=0;
+	int ballflgy=0;
+	// Use this for initialization
+	void Start () {
+		ballp = ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ballflgy == 0) {
+			ballp.y -= 0.1f;
+		} else {
+			ballp.y += 0.1f;
+		}
+		if (ballflgx == 0) {
+			ballp.x -= 0.1f;
+		} else {
+			ballp.x += 0.1f;
+		}
+		if (ballp.x >= 11f) {
+			ballflgx = 0;
+		}
+		if(ballp.x<=-11f){
+			ballflgx=1;
+		}
+		if (ballp.y >= 13.7f) {
+			ballflgy = 0;
+		}
+		ball.transform.position = ballp;
+	}
+	void OnCollisionEnter(Collision other){
+		ballflgy = 1 ;
+	}
+}
