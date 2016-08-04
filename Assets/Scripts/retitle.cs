3d599eae8521f1b59e156cbbe32fafadf690348e
nwuser <nwuser@da.iwasaki.ac.jp>
2016-08-04 16:53:57 +0900

    スクリプトとプレハブの追加♡

---
+++ Assets/Scripts/retitle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class no : MonoBehaviour {
+
+	public float time = 0;
+	private float st_time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		time += Time.deltaTime;
+		if (time >= st_time) {
+			
+			Application.LoadLevel ("Title");
+		}
+	
+	}
+}
